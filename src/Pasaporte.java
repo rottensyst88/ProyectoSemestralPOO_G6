d85ffe18613ccb6cf3661b9f31659a7ab438feca
rottensyst88 <arib042@yahoo.com>
2024-09-05 23:30:59 -0400

    Se cargan las clases Pasaporte y Rut al develop ~AB

--- src/Pasaporte.java
+++ src/Pasaporte.java
@@ -1,3 +1,21 @@
 public class Pasaporte {
-    private String nombre;
+    private String numero;
+    private String nacionalidad;
+
+    private Pasaporte(String num, String nacionalidad) {
+        this.numero = num;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public String getNacionalidad() {
+        return this.nacionalidad;
+    }
+
+    public Pasaporte of(String num, String nacionalidad) {
+        return new Pasaporte(num, nacionalidad);
+    }
 }
