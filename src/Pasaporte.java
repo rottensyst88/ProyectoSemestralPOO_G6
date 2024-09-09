76cfa9fbfd1e478914d19fee871cb6178b131039
rottensyst88 <arib042@yahoo.com>
2024-09-09 00:56:17 -0300

    Todas las clases deberían estar casi terminadas, salvo pequeños detalles puntuales y de fácil arreglo.
    La interfaz idPersona esta lista.
    
    ~AB

--- src/Pasaporte.java
+++ src/Pasaporte.java
@@ -1,10 +1,10 @@
-public class Pasaporte {
+public class Pasaporte implements IdPersona {
     private String numero;
     private String nacionalidad;
 
     private Pasaporte(String num, String nacionalidad) {
         this.numero = num;
-        this.nacionalidad = nacionalidad;
+        this.nacionalidad = nacionalidad.substring(0, 1).toUpperCase() + nacionalidad.substring(1); // ¡HABLAR CON EL PROFESOR!
     }
 
     public String getNumero() {
@@ -15,7 +15,23 @@ public class Pasaporte {
         return this.nacionalidad;
     }
 
+    public String toString() {
+        return (this.numero + " " + this.nacionalidad);
+    }
+
+    public Boolean equals(Pasaporte otro) {
+        return (this.numero.equalsIgnoreCase(otro.numero) && this.nacionalidad.equalsIgnoreCase(otro.nacionalidad)); // ¡HABLAR CON EL PROFESOR!
+    }
+
     public Pasaporte of(String num, String nacionalidad) {
         return new Pasaporte(num, nacionalidad);
     }
 }
+
+/* SI LEES ESTO, PREGUNTARLE AL PROFESOR SOBRE EL FORMATO A UTILIZAR
+ * UNA COSA ES Chile Y OTRA COSA ES CHILE    !
+ * EN EL CONSTRUCTOR DE PASAPORTE, LA PRIMERA LETRA DE LA NACIONALIDAD SERÁ SIEMPRE MAYÚSCULA
+ * EN EL MÉTODO DE EQUALS, SE IGNORAN LAS CAPITALIZACIONES, PERO MEJOR PREGUNTARLE AL PROFESOR!
+ *
+ * ~AB
+ * */
--- src/Rut.java
+++ src/Rut.java
@@ -1,8 +1,8 @@
-public class Rut {
+public class Rut implements IdPersona {
     private int numero;
     private char dv;
 
-    private Rut (int num, char dv){
+    private Rut(int num, char dv) {
         this.numero = num;
         this.dv = dv;
     }
@@ -15,10 +15,34 @@ public class Rut {
         return dv;
     }
 
-    public Rut of (String rutConDv){
-        String[] rut_y_dv = rutConDv.split("-");
-        rut_y_dv[0] = rut_y_dv[0].replace(".","");
+    public String toString() {
+        String num_conv = String.valueOf(numero);
+        if (num_conv.length() == 8) { // SE REALIZA UNA DIFERENCIACIÓN ENTRE LOS RUTS DE 7 U 8 DIGITOS
+            return (num_conv.substring(0, 2) + "." + num_conv.substring(2, 5) + "." + num_conv.substring(5, 8) + "-" + dv); // RUT 8 DIGITOS
+        } else {
+            return (num_conv.charAt(0) + "." + num_conv.substring(1, 4) + "." + num_conv.substring(4, 7) + "-" + dv); // RUT 7 DIGITOS
+        }
+    }
 
-        return new Rut(Integer.parseInt(rut_y_dv[0]),rut_y_dv[1].charAt(0));
+    public Boolean equals(Rut otro) {
+        return (this.numero == otro.numero && this.dv == otro.dv);
+    }
+
+    public Rut of(String rutConDv) {
+        String[] rut_y_dv = rutConDv.split("-"); // DIVIDE STRING EN DOS PEDAZOS (.... - ... )
+        rut_y_dv[0] = rut_y_dv[0].replace(".", ""); // ELIMINA LOS PUNTOS
+
+        return new Rut(Integer.parseInt(rut_y_dv[0]), rut_y_dv[1].charAt(0)); // CREA NUEVO RUT EN BASE AL STRING ANTERIOR
     }
 }
+
+/* ESTA CLASE DEBERÍA ESTAR TERMINADA! POR FAVOR, EVITEN MOVERLE COSAS
+    RECUERDEN EL DICHO...
+
+    SI FUNCIONA, NO LO TOQUE
+
+    VAN A SER LAS 1 DE LA MAÑANA, NO ME HAGAN ARREGLAR PORQUERÍAS QUE YO NO CORROMPÍ!
+
+    (-:
+    ~AB
+ */
