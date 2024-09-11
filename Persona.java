b187ab6112230c6e7b420997476a3ba4e32f9779
rottensyst88 <arib042@yahoo.com>
2024-09-11 19:31:52 -0300

    se sube la clase persona

---
+++ Persona.java
@@ -0,0 +1,58 @@
+public class Persona {
+    public class Persona {
+        // Atributos
+        private IdPersona idPersona;
+        private Nombre nombreCompleto;
+        private String telefono;
+
+        // Constructor
+        public Persona(IdPersona id, Nombre nombre) {
+            this.idPersona = id;
+            this.nombreCompleto = nombre;
+        }
+
+        // Getters y Setters
+        public IdPersona getIdPersona() {
+            return idPersona;
+        }
+
+        public void setIdPersona(IdPersona idPersona) {
+            this.idPersona = idPersona;
+        }
+
+        public Nombre getNombreCompleto() {
+            return nombreCompleto;
+        }
+
+        public void setNombreCompleto(Nombre nombreCompleto) {
+            this.nombreCompleto = nombreCompleto;
+        }
+
+        public String getTelefono() {
+            return telefono;
+        }
+
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+
+        // Método toString
+        @Override
+        public String toString() {
+            return "Persona[idPersona=" + idPersona + ", nombreCompleto=" + nombreCompleto + ", telefono=" + telefono + "]";
+        }
+
+        // Método equals
+        @Override
+        public boolean equals(Object otro) {
+            if (this == otro) {
+                return true;
+            }
+            if (otro == null || getClass() != otro.getClass()) {
+                return false;
+            }
+            Persona persona = (Persona) otro;
+            return idPersona.equals(persona.idPersona);
+        }
+    }
+}
