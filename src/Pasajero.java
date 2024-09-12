083b701b9ccacc2b80ba5fc95efa0eed86c0bce1
RZZZZZZ <sebaruizinsuco@gmail.com>
2024-09-12 10:30:00 -0300

    Se crea clase pasajero y se agrega a develop

---
+++ src/Pasajero.java
@@ -0,0 +1,21 @@
+public class Pasajero extends Persona{
+
+    private Nombre nomContacto;
+    private String fonoContacto;
+
+    public Nombre getNomContacto() {
+        return nomContacto;
+    }
+
+    public void setNomContacto(Nombre nomContacto) {
+        this.nomContacto = nomContacto;
+    }
+
+    public String getFonoContacto() {
+        return fonoContacto;
+    }
+
+    public void setFonoContacto(String fonoContacto) {
+        this.fonoContacto = fonoContacto;
+    }
+}
