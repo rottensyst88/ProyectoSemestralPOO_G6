90739be55dacb812acfa2383f3b1f958a3426a30
rottensyst88 <arib042@yahoo.com>
2024-09-15 01:16:10 -0300

    Se juntan todos los avances de los integrantes a Develop!
    ~AB

--- src/Persona.java
+++ src/Persona.java
@@ -1,59 +1,58 @@
 public class Persona {
-    public class Persona {
-        // Atributos
-        private IdPersona idPersona;
-        private Nombre nombreCompleto;
-        private String telefono;
+    // Atributos
+    private IdPersona idPersona;
+    private Nombre nombreCompleto;
+    private String telefono;
 
-        // Constructor
-        public Persona(IdPersona id, Nombre nombre) {
-            this.idPersona = id;
-            this.nombreCompleto = nombre;
-        }
+    // Constructor
+    public Persona(IdPersona id, Nombre nombre) {
+        this.idPersona = id;
+        this.nombreCompleto = nombre;
+    }
 
-        // Getters y Setters
-        public IdPersona getIdPersona() {
-            return idPersona;
-        }
+    // Getters y Setters
+    public IdPersona getIdPersona() {
+        return idPersona;
+    }
 
-        public void setIdPersona(IdPersona idPersona) {
-            this.idPersona = idPersona;
-        }
+    public void setIdPersona(IdPersona idPersona) {
+        this.idPersona = idPersona;
+    }
 
-        public Nombre getNombreCompleto() {
-            return nombreCompleto;
-        }
+    public Nombre getNombreCompleto() {
+        return nombreCompleto;
+    }
 
-        public void setNombreCompleto(Nombre nombreCompleto) {
-            this.nombreCompleto = nombreCompleto;
-        }
+    public void setNombreCompleto(Nombre nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
 
-        public String getTelefono() {
-            return telefono;
-        }
+    public String getTelefono() {
+        return telefono;
+    }
 
-        public void setTelefono(String telefono) {
-            this.telefono = telefono;
-        }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 
-        // Método toString
-        @Override
-        public String toString() {
-            return "Persona[idPersona=" + idPersona + ", nombreCompleto=" + nombreCompleto + ", telefono=" + telefono + "]";
-        }
+    // Método toString
+    @Override
+    public String toString() {
+        return "Persona[idPersona=" + idPersona + ", nombreCompleto=" + nombreCompleto + ", telefono=" + telefono + "]";
+    }
 
-        // Método equals
-        @Override
-        public boolean equals(Object otro) {
-            if (this == otro) {
-                return true;
-            }
-            if (otro == null || getClass() != otro.getClass()) {
-                return false;
-            }
-            Persona persona = (Persona) otro;
-            return idPersona.equals(persona.idPersona);
+    // Método equals
+    @Override
+    public boolean equals(Object otro) {
+        if (this == otro) {
+            return true;
         }
+        if (otro == null || getClass() != otro.getClass()) {
+            return false;
+        }
+        Persona persona = (Persona) otro;
+        return idPersona.equals(persona.idPersona);
     }
 }
+
 //falta revisar el to string, equals.
