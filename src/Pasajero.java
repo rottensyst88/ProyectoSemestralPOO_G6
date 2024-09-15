6fe99f7283618ee8aee59b59be042d1fef25e472
rottensyst88 <arib042@yahoo.com>
2024-09-15 01:39:21 -0300

    Se agrego un constructor a Pasajero. Confirmar con el profesor!
    ~AB

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -3,6 +3,12 @@ public class Pasajero extends Persona{
     private Nombre nomContacto;
     private String fonoContacto;
 
+    public Pasajero(IdPersona id, Nombre nom, String fonoContacto){ // REVISAR CON EL PROFESOR!
+        super(id, nom);
+        this.nomContacto = nom;
+        this.fonoContacto = fonoContacto;
+    }
+
     public Nombre getNomContacto() {
         return nomContacto;
     }
