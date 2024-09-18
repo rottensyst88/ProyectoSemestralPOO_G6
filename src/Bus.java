dde7e690c746d7f66bdc2bc6ccc19107af67a513
rottensyst88 <arib042@yahoo.com>
2024-09-17 21:35:11 -0300

    Se crea la clase Bus, es necesaria crear la clase Viaje! ~AB

--- src/Bus.java
+++ src/Bus.java
@@ -1,7 +1,46 @@
+import java.util.ArrayList;
+
 public class Bus {
     private String patente;
     private String marca;
     private String modelo;
-    private String nroAsientos;
+    private int nroAsientos;
+
+    ArrayList<Viaje> viajes = new ArrayList();
+
+    public Bus(String patente, int nroAsientos){
+        this.patente = patente;
+        this.nroAsientos = nroAsientos;
+        marca = "NN";
+        modelo = "NN";
+    }
+
+    public String getPatente(){
+        return patente;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public int getNroAsientos(){
+        return nroAsientos;
+    }
+
+    public void addViaje(Viaje viaje){
+        viajes.add(viaje);
+    }
 
 }
