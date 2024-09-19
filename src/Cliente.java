029b61c85a5981eed2383aec878ebc51ce49f922
Diego <diego.mercado2401@alumnos.ubiobio.cl>
2024-09-19 12:31:58 -0300

    agrego la clase cliente a mi carpeta, ya que solo estaba en develop

---
+++ src/Cliente.java
@@ -0,0 +1,18 @@
+
+public class Cliente extends Persona{
+    private String email;
+
+    public Cliente(IdPersona id, Nombre nom, String email){
+        super(id, nom);
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
