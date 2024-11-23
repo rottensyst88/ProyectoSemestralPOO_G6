8d29e7e26aea3f39e0de82c1fab96766d46f6c45
rottensyst88 <arib042@yahoo.com>
2024-11-23 01:50:05 -0300

    Se intenta programar una de las opciones... Es más complicado de lo que pensé, pero tengo una idea. ~AB

--- src/persistencia/IOSVP.java
+++ src/persistencia/IOSVP.java
@@ -1,4 +1,5 @@
 package persistencia;
+import controlador.SistemaVentaPasajes;
 import utilidades.*;
 import modelo.*;
 
@@ -13,13 +14,107 @@ public class IOSVP implements Serializable {
     }
 
     public Object[] readDatosIniciales(){
-        ObjectInputStream archivo = null;
+
+
+        List<Object> objetos = new ArrayList<>();
+        List<String> datosArchivo = new ArrayList<>();
+        Scanner archivo = null;
+        byte contador_pos = 0;
 
         try{
-            archivo = new ObjectInputStream(new FileInputStream ("archivo.obj"));
+            archivo = new Scanner(new File("SVPDatosIniciales.txt"));
 
-        } catch (Exception e){
-            e.printStackTrace();
+            archivo.useLocale(Locale.ENGLISH);
+            while(archivo.hasNextLine()){
+                datosArchivo.add(archivo.next());
+            }
+
+            for(String linea : datosArchivo){
+
+            }
+
+
+            // SECCION DE CLIENTES O PASAJEROS
+            while(archivo.hasNextLine()){
+                String[] temporal = archivo.nextLine().split(";");
+                if(archivo.next().equals("+")){
+                    contador_pos++;
+                    break; // CONTADOR DE POSICIONES
+                }
+                switch(temporal[0]){
+                    case "CP":{ //CLIENTE Y PASAJERO
+                        Nombre nombre = new Nombre();
+                        Nombre nContacto = new Nombre();
+                        IdPersona id = Rut.of(temporal[1]);
+                        nombre.setTratamiento(Enum.valueOf(Tratamiento.class, temporal[2]));
+                        nombre.setNombre(temporal[3]);
+                        nombre.setApellidoPaterno(temporal[4]);
+                        nombre.setApellidoMaterno(temporal[5]);
+                        String tel = temporal[6];
+                        String email = temporal[7];
+                        nContacto.setTratamiento(Enum.valueOf(Tratamiento.class, temporal[8]));
+                        nContacto.setNombre(temporal[9]);
+                        nContacto.setApellidoPaterno(temporal[10]);
+                        nContacto.setApellidoMaterno(temporal[11]);
+                        String telContacto = temporal[12];
+
+                        Cliente c = new Cliente(id,nombre,email);
+                        c.setTelefono(tel);
+
+                        Pasajero p = new Pasajero(id,nombre,tel);
+                        p.setTelefono(tel);
+                        p.setNomContacto(nContacto);
+                        p.setFonoContacto(telContacto);
+                        break;
+                    }
+                    case "C":{ //CLIENTE
+                        Nombre nombre = new Nombre();
+                        IdPersona id = Rut.of(temporal[1]);
+                        nombre.setTratamiento(Enum.valueOf(Tratamiento.class, temporal[2]));
+                        nombre.setNombre(temporal[3]);
+                        nombre.setApellidoPaterno(temporal[4]);
+                        nombre.setApellidoMaterno(temporal[5]);
+                        String tel = temporal[6];
+                        String email = temporal[7];
+
+                        Cliente c = new Cliente(id,nombre,email);
+                        c.setTelefono(tel);
+                        break;
+                    }
+                    case "P":{ //PASAJERO
+                        Nombre nombre = new Nombre();
+                        Nombre nContacto = new Nombre();
+                        IdPersona id = Rut.of(temporal[1]);
+                        nombre.setTratamiento(Enum.valueOf(Tratamiento.class, temporal[2]));
+                        nombre.setNombre(temporal[3]);
+                        nombre.setApellidoPaterno(temporal[4]);
+                        nombre.setApellidoMaterno(temporal[5]);
+                        String tel = temporal[6];
+                        nContacto.setTratamiento(Enum.valueOf(Tratamiento.class, temporal[7]));
+                        nContacto.setNombre(temporal[8]);
+                        nContacto.setApellidoPaterno(temporal[9]);
+                        nContacto.setApellidoMaterno(temporal[10]);
+                        String telContacto = temporal[11];
+
+                        Pasajero p = new Pasajero(id,nombre,telContacto);
+                        p.setTelefono(tel);
+                        p.setNomContacto(nContacto);
+                        break;
+                    }
+                    default:{
+                        System.out.println("ERROR! ESTO NO DEBERIA PASAR!");
+                    }
+                }
+            }
+            while(archivo.hasNextLine() && )
+
+            while()
+
+
+
+        } catch (FileNotFoundException e){
+            System.out.println("Error! El archivo requerido no ha sido encontrado!");
+            return null;
         }
     }
 
